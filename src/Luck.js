0bc683b044db4182785612f8fde424512eb5f131
starzou <z260142514@qq.com>
2014-09-04 22:04:29 +0800

    Initial commit

---
+++ src/Luck.js
@@ -0,0 +1,13 @@
+/**
+ * @description :
+ * @since : 2014/9/4 21:56
+ * @author : StarZou
+ */
+
+var Luck = Luck || {};
+Luck._startTime = new Date().getTime();
+(function (window, Luck, undefined) {
+
+
+
+})(window, Luck);
