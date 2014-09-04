82c88ab59e32d53178a088ad2f2c929b53e47df2
starzou <z260142514@qq.com>
2014-09-04 22:53:04 +0800

    add Luck

--- src/Luck.js
+++ src/Luck.js
@@ -6,8 +6,42 @@
 
 var Luck = Luck || {};
 Luck._startTime = new Date().getTime();
-(function (window, Luck, undefined) {
+(function (global, Luck, undefined) {
+    var objectPrototype = Object.prototype,
+        toString = objectPrototype.toString;
+
+    Luck.apply = function (object, config, defaults) {
+        if (defaults) {
+            Luck.apply(object, defaults);
+        }
+
+        if (object && config && typeof config === 'object') {
+            var property;
+            for (property in config) {
+                if (config.hasOwnProperty(property)) {
+                    object[property] = config[property];
+                }
+            }
+        }
+
+        return object;
+    };
+
+    Luck.applyIf = function (object, config) {
+        var property;
+
+        if (object) {
+            for (property in config) {
+                if (config.hasOwnProperty(property) && object[property] === undefined) {
+                    object[property] = config[property];
+                }
+            }
+        }
+
+        return object;
+    };
 
 
+    Luck.global = global;
 
 })(window, Luck);
